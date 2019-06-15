092edde2dab27839c04d6d4c8d0fe40a8b0cd94b
Alexander Yudin <geexup@gmail.com>
2019-06-16 01:34:10 +0300

    moment().add(period, number) is deprecated

--- src/scenes/fill-day/helpers/create-clockify-time-range.ts
+++ src/scenes/fill-day/helpers/create-clockify-time-range.ts
@@ -4,7 +4,7 @@ const CLOCKIFY_DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSS[Z]';
 
 export function createClockifyTimeRange(hours: number = 8) {
   const startMoment = moment().utc().set('minute', 0).set('seconds', 0).set('millisecond', 0);
-  const endMoment = moment().utc().set('minute', 0).set('seconds', 0).set('millisecond', 0).add('hour', hours);
+  const endMoment = moment().utc().set('minute', 0).set('seconds', 0).set('millisecond', 0).add(hours, 'hour');
 
   return {
     start: startMoment.format(CLOCKIFY_DATE_FORMAT),
